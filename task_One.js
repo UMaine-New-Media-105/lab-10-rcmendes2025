34e540456a2d6b556861932242e4c956426ff1c3
rcmendes2025 <124199152+rcmendes2025@users.noreply.github.com>
2023-04-19 14:47:00 -0400

    Task One
    https://editor.p5js.org/ryan.mendes1/sketches/nR3-4zofd

---
+++ task_One.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  scene();
+}
+
+function scene() {
+  noStroke();
+  fill("#88FF57")
+  rect(0, 0, 400, 50)
+  fill("#6CD1FF")
+  rect(0, 50, 400, 50)
+  rect(0, 100, 400, 50)
+  fill("#88FF57")
+  rect(0, 150, 400, 50)
+  fill("#2A2A2A")
+  rect(0, 200, 400, 50)
+  rect(0, 250, 400, 50)
+  rect(0, 300, 400, 50)
+  fill("#88FF57")
+  rect(0, 350, 400, 50)
+}
